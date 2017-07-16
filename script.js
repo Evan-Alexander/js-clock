3be3aa66f18b7373e4d415737baf331214b150bb
Jason Brown <jasontbrown99@gmail.com>
2017-07-15 18:13:37 -0700

    fix resset second hand to zero issue

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
+const HOURHAND = document.querySelector("#hour");
+const MINUTEHAND = document.querySelector("#minute");
+const SECONDHAND = document.querySelector("#second");
+
+var date = new Date();
+console.log(date);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log("Hour: " + hr + " Minute: " + min + " Seconds: " + sec);
+
+let hrPosition = (hr*360/12)+(min*(360/60)/12);
+let minPosition = (min*360/60)+(sec*(360/60)/60);
+let secPostion = sec*360/60;
+
 
 function runTheClock() {
-  const HOURHAND = document.querySelector("#hour");
-  const MINUTEHAND = document.querySelector("#minute");
-  const SECONDHAND = document.querySelector("#second");
 
-  var date = new Date();
-  console.log(date);
-  let hr = date.getHours();
-  let min = date.getMinutes();
-  let sec = date.getSeconds();
-  console.log("Hour: " + hr + " Minute: " + min + " Seconds: " + sec);
-
-  let hrPosition = (hr*360/12)+(min*(360/60)/12);
-  let minPosition = (min*360/60)+(sec*(360/60)/60);
-  let secPostion = sec*360/60;
-
+  hrPosition = hrPosition + (3/360);
+  minPosition = minPosition + (6/60);
+  secPostion = secPostion + 6;
 
   HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
   MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
